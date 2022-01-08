290b466c44251945fcf7b7bb38d4fc514b4b1485
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2022-01-07 22:55:19 -0600

    borders ButtonTag

--- src/components/ButtonTagHome.js
+++ src/components/ButtonTagHome.js
@@ -4,7 +4,7 @@ function ButtonTagHome({ children }) {
   return (
     <button
       type="button"
-      className={`mx-1 hover:bg-slate-100 font-small rounded-lg text-sm px-2 py-1.5 text-center`}
+      className={`mx-1 hover:bg-slate-100 hover:border border-gray-400 font-small rounded-lg text-sm px-2 py-1.5 text-center`}
     >
       {children}
     </button>
